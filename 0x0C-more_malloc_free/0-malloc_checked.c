ad5b0c12ac5a4502c23bf5d69755826826626214
mu-rad <muradabdulrahman533@gmail.com>
2023-11-02 15:34:28 -0700

    Write a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,89 +1,25 @@
 #include "main.h"
-
-                
-
 #include <stdlib.h>
 
-                
-
 /**
+* malloc_checked - cause normal process termination with a status value of 98
+* @b: allocated memory
+* Return: a pointer to the allocated memory
+*/
 
-                
-
- * create_array - create array of size size and assign char c
-
-                
-
- * @size: size of array
-
-                
-
- * @c: char to assign
-
-                
-
- * Description: creat array of size size and assign char c
-
-                
-
- * Return: pointer to array, NULL if fail
-
-                
-
- *
-
-                
-
- */
-
-                
-
-char *create_array(unsigned int size, char c)
-
-                
+void *malloc_checked(unsigned int b)
 
 {
 
-                
-
-        char *str;
-
-                
-
-        unsigned int i;
-
-                
+   void *i;
 
 
-                
+   i = malloc(b);
 
-        str = malloc(sizeof(char) * size);
+   if (i == NULL)
 
-                
+       exit(98);
 
-        if (size == 0 || str == NULL)
-
-                
-
-                return (NULL);
-
-                
-
-
-                
-
-        for (i = 0; i < size; i++)
-
-                
-
-                str[i] = c;
-
-                
-
-        return (str);
-
-                
+   return (i);
 
 }
-
-
